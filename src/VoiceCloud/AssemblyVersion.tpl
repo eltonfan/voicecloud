72ca5de507849e66cdc6d6a28efa2caf9f0d32e4
范传根 <chuangen@live.cn>
2014-10-03 22:50:14 +0800

    添加版本描述文件；更改NAudio库为已签名版本。
    移除NuGet管理的NAudio（因为没有强名称签名），添加自行编译的已签名版本。

---
+++ src/VoiceCloud/AssemblyVersion.tpl
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: AssemblyCompany("MAVPLUS")]
+[assembly: AssemblyProduct("VoiceCloud.NET")]
+[assembly: AssemblyCopyright("\u00A9 2014 MAVPLUS. http://www.mavplus.com/")]
+[assembly: AssemblyTrademark("MAVPLUS")]
+
+[assembly: AssemblyVersion("1.0.0.$REV$")]
+[assembly: AssemblyFileVersion("1.0.0.$REV$")]
+[assembly: AssemblyInformationalVersion("v1.0.0 Alpha")]
+[assembly: AssemblyDefaultAlias("$HASH$ $DATE$")]
--- src/VoiceCloud/Mavplus.VoiceCloud/Properties/AssemblyInfo.cs
+++ src/VoiceCloud/Mavplus.VoiceCloud/Properties/AssemblyInfo.cs
@@ -5,13 +5,8 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过以下
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("Mavplus.VoiceCloud")]
+[assembly: AssemblyTitle("VoiceCloud.NET")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Mavplus.VoiceCloud")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // 将 ComVisible 设置为 false 使此程序集中的类型
@@ -21,16 +16,3 @@ using System.Runtime.InteropServices;
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
 [assembly: Guid("4ff2bab2-f106-4195-9e3e-2a6eaf274204")]
-
-// 程序集的版本信息由下面四个值组成: 
-//
-//      主版本
-//      次版本 
-//      生成号
-//      修订号
-//
-// 可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
-// 方法是按如下所示使用“*”: 
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
